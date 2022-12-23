4864479dfee3b38cf865aab2aef61962c960661c
Alalaq <alisher.khabibullin@gmail.com>
2022-12-24 00:59:13 +0300

    FINALLY READINESS IS WORKING

--- Server/src/main/java/ru/kpfu/itis/listeners/ReadinessListener.java
+++ Server/src/main/java/ru/kpfu/itis/listeners/ReadinessListener.java
@@ -1,7 +1,6 @@
 package ru.kpfu.itis.listeners;
 
 import com.google.gson.JsonSyntaxException;
-import org.apache.commons.lang.ArrayUtils;
 import ru.kpfu.itis.general.entities.Player;
 import ru.kpfu.itis.general.entities.Room;
 import ru.kpfu.itis.general.helpers.general.DrawingCode;
@@ -10,10 +9,8 @@ import ru.kpfu.itis.protocol.Constants;
 import ru.kpfu.itis.protocol.Message;
 import ru.kpfu.itis.server.Connection;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 public class ReadinessListener extends AbstractServerEventListener {
     public ReadinessListener() {
@@ -57,13 +54,20 @@ public class ReadinessListener extends AbstractServerEventListener {
         for (Connection connection : connectionList) {
             Player player = connection.getPlayer();
             if (player.getRoom().equals(room)) {
-                for (long i = 0L; i < drawings.size(); i++) {
-                    if (!Objects.equals(player.getId(), i)) {
-                        code = DrawingCode.builder()
-                                .round(room.getCurrentRound())
-                                .playerId(i)
-                                .isUsed(false)
-                                .build();
+                    if (player.getReadiness()) {
+                        if (!player.getId().equals((long) room.getPlayers().size() - 1)) {
+                            code = DrawingCode.builder()
+                                    .round(room.getCurrentRound())
+                                    .playerId(player.getId() + 1L)
+                                    .isUsed(false)
+                                    .build();
+                        } else {
+                            code = DrawingCode.builder()
+                                    .round(room.getCurrentRound())
+                                    .playerId(0L)
+                                    .isUsed(false)
+                                    .build();
+                        }
                         if (drawings.containsKey(code)) {
                             byte[] drawingBytes = (drawings.get(code));
                             message = new Message(Constants.NEXT_ROUND, drawingBytes);
@@ -75,8 +79,8 @@ public class ReadinessListener extends AbstractServerEventListener {
                             player.setReadiness(false);
                         }
                     }
-                }
             }
         }
+        room.setCurrentRound(room.getCurrentRound() + 1);
     }
 }
