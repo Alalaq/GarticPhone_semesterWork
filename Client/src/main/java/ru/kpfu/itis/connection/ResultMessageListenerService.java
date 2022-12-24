2e479b2465951e7b87fe72f4f0444135c4454267
Mihail Lovchitskiy <misha_aid@mail.ru>
2022-12-24 20:40:07 +0300

    removed old parser

--- Client/src/main/java/ru/kpfu/itis/connection/ResultMessageListenerService.java
+++ Client/src/main/java/ru/kpfu/itis/connection/ResultMessageListenerService.java
@@ -3,11 +3,8 @@ package ru.kpfu.itis.connection;
 import javafx.application.Platform;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
-import javafx.scene.control.ListView;
-import javafx.scene.layout.VBox;
 import ru.kpfu.itis.general.entities.Drawing;
 import ru.kpfu.itis.general.helpers.parsers.DrawingParser;
-import ru.kpfu.itis.general.helpers.parsers.NewDrawingParser;
 import ru.kpfu.itis.general.helpers.parsers.PlayerParser;
 import ru.kpfu.itis.gui.controllers.ResultController;
 import ru.kpfu.itis.protocol.Constants;
@@ -48,7 +45,7 @@ public class ResultMessageListenerService extends Service<Void> {
                         }
                         case Constants.SENDED_ONE_GAME_BRANCH ->{
                             Platform.runLater(()->{
-                                List<Drawing> drawings = NewDrawingParser.deserializeObjects(message.getBody());
+                                List<Drawing> drawings = DrawingParser.deserializeObjects(message.getBody());
                                 System.out.println(drawings);
                                 resultController.showOneGameBranch(drawings);
                             });
